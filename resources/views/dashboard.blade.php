5d0dedfc9acba933869553c866f885f2f1098748
Nisha <hm1731942@example.com>
2024-06-14 09:20:28 -0800

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,9 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-
+<div>
+     <button type="submit" class="btn btn-danger"> logout</button>
+</div>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
